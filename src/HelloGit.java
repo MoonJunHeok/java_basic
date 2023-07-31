3785f63e89f9b65f18919226348459821619a6b9
vangry313 <vangry313@gamil.com>
2023-07-31 16:40:05 +0900

    homework

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,6 +3,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("11111111111111111111111");
 		System.out.println("222222222222222222222222222222s");
+		System.out.println("클론 집에서 작업을 하였습니다..");
 	}
 
 }
